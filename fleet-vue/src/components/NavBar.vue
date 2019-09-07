cd8513aafb2d1d061a41b77c81bf738176106f1f
Daniel <danieldickson89@gmail.com>
2019-09-07 14:19:11 -0600

    First commit

---
+++ fleet-vue/src/components/NavBar.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <!-- <font-awesome-icon icon="plus"/> -->
+    <div class="h3">
+        NavBar
+    </div>
+  </div>
+</template>
+
+<script>
+
+export default {
+    name: 'NavBar',
+    data() {
+        return {
+
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
